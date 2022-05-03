8135f3509708e97e44f8893265af8a94dae8863a
Cat <56826818+Colinkl@users.noreply.github.com>
2022-05-04 02:39:01 +0700

    Rotation script done

--- Assets/LearnMaterials 2/Scripts/RotationScript.cs
+++ Assets/LearnMaterials 2/Scripts/RotationScript.cs
@@ -5,22 +5,24 @@ using UnityEngine;
 public class RotationScript : BaseScript
 {
     [SerializeField]
-    private const float rotationSpeed = 10f;
+    private const float rotationSpeed = 1f;
 
     [SerializeField]
     private Vector3 vect = new Vector3(0,90,0);
 
     private IEnumerator Rotate()
     {
-        Vector3 r = (vect + transform.eulerAngles ).normalized;
-
-        while (Vector3.Angle(transform.eulerAngles, r) > 2*rotationSpeed * Time.deltaTime)
+        Quaternion r = transform.rotation * Quaternion.Euler(vect);
+        var xxx = Quaternion.Angle(transform.rotation, r);
+        while (Quaternion.Angle(transform.rotation, r) > 0.1*rotationSpeed)
         {
-            transform.Rotate(rotationSpeed * Time.deltaTime * r);
+            var c = transform.rotation.eulerAngles;
+            var rot = vect * rotationSpeed * Time.deltaTime;
+            transform.rotation = Quaternion.Euler(rot) * transform.rotation;
             yield return null;
         }
 
-        transform.eulerAngles = r;
+        transform.rotation = r;
     }   
 
     [ContextMenu("Rotate")]
