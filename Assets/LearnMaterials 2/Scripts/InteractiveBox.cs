61ab9285476e82c593ef92809c97957ea94adb35
Cat <56826818+Colinkl@users.noreply.github.com>
2022-05-04 04:33:59 +0700

    interaction box

---
+++ Assets/LearnMaterials 2/Scripts/InteractiveBox.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Assets.LearnMaterials_2.Scripts
+{
+    public class InteractiveBox : MonoBehaviour
+    {
+        [SerializeField]
+        private InteractiveBox nextItem;
+
+        public void AddNext(InteractiveBox nextBox)
+        {
+            if (nextBox != null)
+                nextItem = nextBox;
+        }
+
+        void Update()
+        {
+            if (nextItem == null)
+                return;
+
+            RaycastHit hit;
+
+            Vector3 direction = nextItem.transform.position - transform.position;
+
+            if (Physics.Raycast(transform.position, direction, out hit))
+            {
+                hit.transform.GetComponent<ObstacleItem>()?.GetDamage(Time.deltaTime);
+            }
+            Debug.DrawRay(transform.position, direction, Color.red);
+        }
+    }
+}
