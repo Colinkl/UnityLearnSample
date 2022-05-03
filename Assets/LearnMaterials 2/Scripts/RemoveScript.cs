384428d6876ad85a2c386783fe488028aba0d91c
Cat <56826818+Colinkl@users.noreply.github.com>
2022-05-04 03:47:40 +0700

    remove script

---
+++ Assets/LearnMaterials 2/Scripts/RemoveScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveScript : BaseScript
+{
+    [SerializeField]
+    [Min(0f)]
+    private float speed = 1f;
+
+    [SerializeField]
+    private GameObject target;
+
+    private Vector3 shrinkV = new Vector3(1, 1, 1);
+    [ContextMenu("Use")]
+    public override void Use()
+    {
+        StartCoroutine(RemoveChilds());
+    }
+
+    public IEnumerator RemoveChilds()
+    {
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            {
+                Transform c = transform.GetChild(i);
+                while (c.localScale.x > 0)
+                {
+                    c.localScale -= shrinkV * Time.deltaTime * speed;
+                    yield return null;
+                }
+
+                Destroy(c.gameObject);
+                yield return null;
+            }
+        }
+    }
+}
