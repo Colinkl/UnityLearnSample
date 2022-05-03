154c2087895b3126643f64f2f62c89c26f91a873
Cat <56826818+Colinkl@users.noreply.github.com>
2022-05-04 04:29:45 +0700

    a

---
+++ Assets/LearnMaterials 2/Scripts/ObstacleItem.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Assets.LearnMaterials_2.Scripts
+{
+    public class ObstacleItem : MonoBehaviour
+    {
+        [Range(0, 1f)]
+        private float health = 1;
+        public UnityEvent onDestroyObstacle;
+
+        public void GetDamage(float value)
+        {
+            health -= value;
+            if (health <= 0)
+            {
+                onDestroyObstacle.Invoke();
+                Destroy(gameObject);
+
+            }
+
+            gameObject.GetComponent<Renderer>().material.color = new Color(1, health, health);
+        }
+
+    }
+}
